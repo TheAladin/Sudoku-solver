523e35fa42b2502e6f9baeaac67d5eec51477193
TheAladin <104937236+TheAladin@users.noreply.github.com>
2023-11-17 23:56:37 -0500

    Add functions structure

---
+++ script.js
@@ -0,0 +1,19 @@
+const checkRow = function() {
+
+}
+
+const checkColumn = function() {
+
+}
+
+const checkSquare = function() {
+
+}
+
+const clearGrid = function() {
+
+}
+
+const insertDigit = function() {
+
+}
