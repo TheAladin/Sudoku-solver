28141ef44c59794c2fdc4121b3c900942a798b75
TheAladin <104937236+TheAladin@users.noreply.github.com>
2023-11-18 23:50:37 -0500

    Add row checking function's body

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const checkRow = function() {
-
+const checkRow = function(rowNumber) {
+  let checkingArray = [];
+  for(let i = 0;i<9;i++) {
+    checkingArray.push(grid[rowNumber][i])
+    checkingArray.sort()
+  }
+  for(let i = 0;i<9;i++) {
+    if (i != checkingArray[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 const checkColumn = function() {
